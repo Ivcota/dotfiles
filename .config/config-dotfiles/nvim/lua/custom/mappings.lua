46b96cc0dd2107352ffd775b6bd66625d74a6a5e
Iverson Diles <ivcotad@gmail.com>
2024-02-11 17:54:27 -0700

    update mappings custom

--- .config/config-dotfiles/nvim/lua/custom/mappings.lua
+++ .config/config-dotfiles/nvim/lua/custom/mappings.lua
@@ -1,5 +1,35 @@
 local M = {}
 
+M.disabled = {
+  n = {
+    ["<leader>v"] = "",
+  }
+}
+
+M.nvimtree = {
+  n = {
+    ["<leader>pv"] = { "<cmd> NvimTreeFocus <CR>", "Focus nvimtree" },
+  }
+}
+
+M.lspconfig = {
+  plugin = true,
+  n = {
+    ["<leader>vrn"] = {
+      function()
+        require("nvchad.renamer").open()
+      end,
+      "LSP rename",
+    },
+    ["<leader>vca"] = {
+      function()
+        vim.lsp.buf.code_action()
+      end,
+      "LSP code action",
+    },
+  }
+}
+
 M.dap = {
   plugin = true,
   n = {
